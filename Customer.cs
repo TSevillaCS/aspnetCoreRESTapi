4ff542399951d2e40e7831adeefa4019cfec7c6b
TSevillaCS <tsevi20@gmail.com>
2020-08-31 08:58:57 -0700

    Create Customer.cs

---
+++ Customer.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebApplication1.Models
+{
+    public class Customer
+    {
+        public string CustomerId { get; set; }
+        
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }   
+
+    }
+}
